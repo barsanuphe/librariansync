78f85426708fb0b0bac3d711f3c3b1fd930317ed
NiLuJe <ninuje@gmail.com>
2014-09-07 23:00:43 +0200

    Visual feedback on export end

--- librariansync/generate_collections.py
+++ librariansync/generate_collections.py
@@ -189,6 +189,9 @@ def export_existing_collections(c):
     with codecs.open(CALIBRE_PLUGIN_FILE, "w", "utf8") as export_json:
         export_json.write(json.dumps(export, sort_keys=True, indent=2, separators=(',', ': '), ensure_ascii = False))
 
+    # Assume it won't fail.
+    kh_msg("Success.", 'I', 'v')
+
 def delete_all_collections(c):
     # build dictionaries of ebooks/collections with their uuids
     db_ebooks, db_collections = parse_entries(c)
