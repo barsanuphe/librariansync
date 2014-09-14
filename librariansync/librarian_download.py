04751bfb8d79e98996158791e67af37d47045c55
Barsanuphe <paixetprosperite@gmail.com>
2014-09-14 13:49:34 +0200

    LibrarianSync can now download and automatically update collections when librarian is serving ebooks.

--- librariansync/librarian_download.py
+++ librariansync/librarian_download.py
@@ -11,24 +11,29 @@
 import requests, os, time, sys
 import ConfigParser
 from kindle_logging import *
+from generate_collections import *
 from urllib import quote
 
 ebook_mimetypes = ['application/epub+zip', 'application/x-mobipocket-ebook']
 
-DESTINATION_DIR = u'/mnt/us/documents/librarian/'
+DESTINATION_DIR = u'/mnt/us/documents/library/'
+COLLECTIONS_DIR = u'/mnt/us/extensions'
 SERVER_HTTP = u"http://%s:%s/"
 
 def url(ip, port, arg):
     return os.path.join(SERVER_HTTP%(ip, port), arg)
 
 def download_file(ip, port, url):
-    filename = url.split(SERVER_HTTP%(ip, port))[1]
-    local_filename = os.path.join(DESTINATION_DIR, filename)
-    if not os.path.exists(os.path.dirname(local_filename)):
-        log(LIBRARIAN_SYNC, "download_file", "Creating %s"%os.path.dirname(local_filename))
-        os.makedirs(os.path.dirname(local_filename))
     r = requests.get(url, stream=True)
-    if r.headers['content-type'] in ebook_mimetypes:
+    if r.headers['content-type'] in ebook_mimetypes or r.headers['content-type'] == "application/json":
+        filename = url.split(SERVER_HTTP%(ip, port))[1]
+        if filename == "collections.json":
+            local_filename = os.path.join(COLLECTIONS_DIR, filename)
+        else:
+            local_filename = os.path.join(DESTINATION_DIR, filename)
+        if not os.path.exists(os.path.dirname(local_filename)):
+            log(LIBRARIAN_SYNC, "download_file", "Creating %s"%os.path.dirname(local_filename))
+            os.makedirs(os.path.dirname(local_filename))
         with open(local_filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk: # filter out keep-alive new chunks
@@ -42,9 +47,11 @@ def download_file(ip, port, url):
         return r.status_code, r.headers['content-type'], None
 
 def download_all_served_ebooks(ip, port):
+    # ask for available ebooks
     code, mimetype, result = download_file(ip, port, url(ip, port, "index"))
     if code == requests.codes.ok and mimetype == "text/plain":
         epubs = result.split("|")
+        # download them all
         for epub in epubs:
             log(LIBRARIAN_SYNC, "download", "D/L: %s."%os.path.basename(epub))
             code, mimetype, result = download_file(ip, port, url(ip, port,epub))
@@ -55,6 +62,9 @@ def download_all_served_ebooks(ip, port):
                     raise Exception("what?")
                 else:
                     log(LIBRARIAN_SYNC, "download", "Downloaded: %s."%os.path.basename(epub), display = False)
+        # ask for the associated collections
+        code, mimetype, result = download_file(ip, port, url(ip, port, "collections.json"))
+        # shutdown the server, all done
         code, mimetype, result = download_file(ip, port, url(ip, port, "LibrarianServer::shutdown"))
         if code == requests.codes.ok and mimetype == "text/plain":
             log(LIBRARIAN_SYNC, "shutdown", result)
@@ -83,6 +93,16 @@ if __name__ == "__main__":
     if failed == len(IPs):
         log(LIBRARIAN_SYNC, "connect", "Impossible to connect to librarian.", "E")
     else:
+        # update collections
+        #subprocess.call(["dbus-send", "--system", "/default", "com.lab126.powerd.resuming int32:1"])
+        try:
+            with sqlite3.connect(KINDLE_DB_PATH) as cc_db:
+                c = cc_db.cursor()
+                log(LIBRARIAN_SYNC, "update", "Updating collections...")
+                update_cc_db(c, complete_rebuild = False, source = "librarian")
+        except:
+            log(LIBRARIAN_SYNC, "main", "Something went wrong while updating collections.", "E")
+
         log(LIBRARIAN_SYNC, "download", "Done in %.02fs."%(time.time()-start))
         # Take care of buffered IO & KUAL's IO redirection...
         sys.stdout.flush()
