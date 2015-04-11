564bed03205866d7e41d2e2c65cb0aa1319c1f6d
Barsanuphe <paixetprosperite@gmail.com>
2015-04-11 23:22:02 +0200

    Fixed creating package.

--- tools/build-librariansync-bin.sh
+++ tools/build-librariansync-bin.sh
@@ -12,17 +12,18 @@ PKGVER=$1
 DEVICE="kindle5"
 
 # check for kindletool
-if (( $(kindletool version | wc -l) != 1 )) ; then
+if (( $(/usr/bin/kindletool version | wc -l) != 1 )) ; then
     echo "KindleTool (https://github.com/NiLuJe/KindleTool) needed to build this package."
     exit 1
 fi
 
 # create tar.gz
 cp -R ../librariansync .
+cp ../README.md .
 tar -zcvf librariansync.tar.gz \
     librariansync/generate_collections.py \
     librariansync/menu.json \
-    librariansync/README.md \
+    README.md \
     librariansync/config.xml \
     librariansync/kindle_contents.py \
     librariansync/kindle_logging.py \
@@ -33,10 +34,9 @@ tar -zcvf librariansync.tar.gz \
 sed -i "s/<version>1.0<\/version>/<version>${PKGVER}<\/version>/g" librariansync/config.xml
 
 # build the update
-kindletool create ota2 -d ${DEVICE} librariansync.tar.gz install.sh Update_${PKGNAME}_${PKGVER}_${DEVICE}.bin
+/usr/bin/kindletool create ota2 -d ${DEVICE} librariansync.tar.gz install.sh Update_${PKGNAME}_${PKGVER}_${DEVICE}.bin
 
 # create release archive
-cp librariansync/README.md .
 tar -zcvf librariansync-${PKGVER}.tar.gz Update_${PKGNAME}_${PKGVER}_${DEVICE}.bin README.md
 
 # cleanup
